b9f18f8c2b33ac4c8c7bf7d2973e079b19375312
Rishabh <rishabhaj13@gmail.com>
2023-03-25 21:06:46 +0530

    solution

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -17,5 +19,36 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
 
+        Payment payment = new Payment();
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        int price = reservation.getSpot().getPricePerHour();
+        int bill = reservation.getNumberOfHours()*price;
+        if(amountSent < bill)
+        {
+            throw new Exception("Insufficient Amount");
+        }
+        if(mode.equalsIgnoreCase("CASH") || mode.equalsIgnoreCase("CARD")|| mode.equalsIgnoreCase("UPI"))
+        {
+            if(mode.equalsIgnoreCase("CASH"))
+            {
+                payment.setPaymentMode(PaymentMode.CASH);
+            }
+            else if (mode.equalsIgnoreCase("CARD"))
+            {
+                payment.setPaymentMode(PaymentMode.CARD);
+            } else if (mode.equalsIgnoreCase("UPI")) {
+                payment.setPaymentMode(PaymentMode.UPI);
+
+            }
+            payment.setPaymentCompleted(true);
+            payment.setReservation(reservation);
+            reservation.setPayment(payment);
+            reservationRepository2.save(reservation);
+            return payment;
+        }
+        else
+        {
+            throw new Exception("Payment mode not detected");
+        }
     }
 }
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -9,6 +9,7 @@ import com.driver.services.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,5 +25,77 @@ public class ReservationServiceImpl implements ReservationService {
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
 
+        try {
+            Reservation reservation = new Reservation();
+            User user = null;
+            ParkingLot parkingLot = null;
+            user = userRepository3.findById(userId).get();
+            // set reservation with user
+            reservation.setUser(user);
+            //  reservation.setPayment(null);
+            parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+
+            List<Spot> spotList = parkingLot.getSpotList();
+
+
+            if (user == null && parkingLot == null) {
+                reservation.setSpot(null);
+
+                reservationRepository3.save(reservation);
+
+                throw new Exception("Cannot make reservation");
+            }
+            int minimumPrice = Integer.MAX_VALUE;
+            Spot spotReq = null;
+            for (Spot spot : spotList) {
+                if (spot.isOccupied() == false) {
+                    if (typeOfSpot(spot.getSpotType()) >= numberOfWheels && spot.getPricePerHour() * timeInHours < minimumPrice) {
+                        minimumPrice = spot.getPricePerHour() * timeInHours;
+                        spotReq = spot;
+
+                    }
+                }
+
+            }
+            if (spotReq == null) {
+                reservation.setSpot(null);
+                reservationRepository3.save(reservation);
+                throw new Exception("Cannot make reservation");
+            }
+
+            // set attributes of reservation
+            reservation.setNumberOfHours(timeInHours);
+            reservation.setSpot(spotReq);
+
+            // set user attributes
+            List<Reservation> reservationList = user.getReservationList();
+            if (reservationList == null) {
+                reservationList = new ArrayList<>();
+            }
+            reservationList.add(reservation);
+            user.setReservationList(reservationList);
+            // set spot attributes
+            List<Reservation> reservations = spotReq.getReservationList();
+            if (reservations == null) {
+                reservations = new ArrayList<>();
+            }
+            reservations.add(reservation);
+            spotReq.setReservationList(reservations);
+            spotReq.setOccupied(true);
+            userRepository3.save(user);
+            spotRepository3.save(spotReq);
+            return reservation;
+
+        } catch (Exception e) {
+            return  null;
+        }
+    }
+    public int typeOfSpot(SpotType spotType)
+    {
+        if(spotType.equals(SpotType.TWO_WHEELER))
+            return 2;
+        else if(spotType.equals(SpotType.FOUR_WHEELER))
+            return 4;
+        return Integer.MAX_VALUE;
     }
 }
